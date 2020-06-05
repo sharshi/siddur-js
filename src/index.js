172989ed49fab0ddde1a5629416d1c9bad3532ce
sharshi <sharshib@gmail.com>
2020-06-05 19:03:36 -0400

    export holiday

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Holiday from "../util/holiday";
 import "./css.scss";
 
-const yo = new Holiday(new Date(2020, 4, 29));
+// const yo = new Holiday(new Date(2020, 4, 29));
 
-console.log(yo.toString());
+// console.log(yo.toString());
+
+export default Holiday;
